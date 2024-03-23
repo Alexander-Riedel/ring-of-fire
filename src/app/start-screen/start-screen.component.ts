03968a65ce37d997e9a22ef8b00b10b4abb2a729
Alexander Riedel <alexander.riedel@gus-group.com>
2024-03-23 17:20:46 +0100

    add game component

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
+
 export class StartScreenComponent {
 
+  constructor(private router: Router) {
+
+  }
+
+  newGame() {
+    // Start game
+    this.router.navigateByUrl('/game');
+  }
+
 }
+

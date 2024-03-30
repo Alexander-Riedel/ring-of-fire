20386ab48fb8c683553090267d1afafb6202da63
Alexander Riedel <alexander.riedel@gus-group.com>
2024-03-30 11:24:28 +0100

    add dialog add player

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule, FormsModule, MatInputModule],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss'
 })
 export class DialogAddPlayerComponent {
+  name: string = '';
+
+  constructor() {
+
+  }
+
+  onNoClick() {
+    
+  }
 
 }
